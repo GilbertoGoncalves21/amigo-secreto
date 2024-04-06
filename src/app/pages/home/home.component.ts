3a38bf4bc36a401a37979cf1624c905a05edf574
Gilberto <gibagonjunior@gmail.com>
2024-04-06 16:08:14 -0300

    Refactor - preparando tela para próximas atualizações

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,7 +8,5 @@ import { Component } from '@angular/core';
 export class HomeComponent {
 
   constructor() { }
-
-  ngOnInit(): void {  }
   
 }
--- src/app/pages/random-draw/random-draw.component.ts
+++ src/app/pages/random-draw/random-draw.component.ts
@@ -43,6 +43,10 @@ export class RandomDrawComponent {
     return this._message
   }
 
+  set name(value: string) {
+    this._name = value.trim();
+  }
+
   addName(nome: string) {
     if (nome == null || nome == '') {
       return;
@@ -62,74 +66,15 @@ export class RandomDrawComponent {
   }
 
   executeDraw(): void {
-    // Embaralhar aleatoriamente a lista de nomes
-    const jumbledNames = this.shuffle([...this._names]);
-
-    // Criar uma cópia da lista de nomes para o sorteio
-    let namesForDraw = [...jumbledNames];
-
-    // Se a quantidade de pessoas for ímpar, remova uma pessoa aleatória do sorteio
-    if (namesForDraw.length % 2 !== 0) {
-      const indiceRemovido = Math.floor(
-        Math.random() * namesForDraw.length
-      );
-      namesForDraw.splice(indiceRemovido, 1);
-    }
-
-    // Realizar o sorteio e atribuir cada pessoa a outra pessoa na lista, excluindo a própria pessoa
-    const draw: { person: string; secretFriend: string; code: string }[] =
-      [];
-    for (let i = 0; i < namesForDraw.length; i++) {
-      const person = namesForDraw[i];
-      let cadidates = [...namesForDraw];
-      cadidates.splice(i, 1); // Remover a própria pessoa da lista de candidatos
-      const indexDraw = Math.floor(Math.random() * cadidates.length);
-      const secretFriend = cadidates[indexDraw];
-      const code = this.generateUniqueCode();
-      draw.push({ person: person, secretFriend: secretFriend, code: code });
-    }
-
-    // Exibir o sorteio
-    console.log('Sorteio de amigo secreto:');
-    draw.forEach((itemDraw) => {
-      console.log(
-        `${itemDraw.person} -> ${itemDraw.secretFriend} (Código: ${itemDraw.code})`
-      );
-    });
   }
 
   cleanNameList(): void {
-    // Limpa a lista de nomes
     this._names = [];
     this.updateDataSource();
   }
 
   private updateDataSource() {
     this._dataSource.data = [...this._names];
-  }
-
-  private generateUniqueCode(): string {
-    return 'COD' + Math.floor(Math.random() * 1000);
-  }
-
-  private shuffle(array: any[]): any[] {
-    let currentIndex = array.length,
-      temporaryValue,
-      randomIndex;
-
-    // Enquanto ainda houver elementos para embaralhar...
-    while (currentIndex !== 0) {
-      // Escolha um elemento não embaralhado restante...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-
-      // E troque-o pelo elemento atual.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-
-    return array;
   }
 }
 
