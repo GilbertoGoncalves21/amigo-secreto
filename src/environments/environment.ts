67156b4d9105ee8e2e7fbd9d215e72f85b0db49f
GilbertoGoncalves21 <gibagonjunior@gmail.com>
2025-02-10 12:52:28 -0300

    Feat - Configurado Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-    production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBQ0U5hCDhViBdBIxxk4qDFh1YC6LzfIc4',
+    authDomain: 'amigo-secreto-67348.firebaseapp.com',
+    projectId: 'amigo-secreto-67348',
+    storageBucket: 'amigo-secreto-67348.firebasestorage.app',
+    messagingSenderId: '356757475872',
+    appId: '1:356757475872:web:a548204438eb7fded52245',
+    measurementId: 'G-W771SXT22H',
+  },
 };
 
 /*
@@ -13,4 +22,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-  // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
